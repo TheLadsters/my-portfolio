ba324a270bd17bbe3c7aa01f47aad104810949bd
Jerald Magsipoc <jeraldmags@gmail.com>
2024-01-08 12:46:43 +0800

    Fix Error in Routing in the landing page:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Main from "./pages/Main";
 
 function App() {
   return (
     <div className="App flex flex-col justify-between min-h-screen">
       <Header />
-      <Main />
+      <Outlet />
       <Footer />
     </div>
   );
--- src/routes/router.js
+++ src/routes/router.js
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
             errorElement: <Error />,
             children: [
                 {
-                    path: "/",
+                    path: "/my-portfolio",
                     element: <Main />,
                 },
             ]

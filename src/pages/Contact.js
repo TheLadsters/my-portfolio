8bbfbb0256001f5b5d33508c86f874c581f06569
Jerald Magsipoc <jeraldmags@gmail.com>
2024-01-15 09:55:14 +0800

    Chang layout of contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,12 +11,12 @@ function Contact() {
 
       <span className='font-light'>Feel free to contact me and get in touch.</span>
 
-      <div className='grid grid-cols-2 gap-4 p-5'>
-            {contacts.map((contact, index) => {
-              return <ContactCard {...contact} key={index} /> 
-            })}
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5'>
+        {contacts.map((contact, index) => {
+          return <ContactCard {...contact} key={index} /> 
+        })}
       </div>
-  </div>
+    </div>
   )
 }
 
